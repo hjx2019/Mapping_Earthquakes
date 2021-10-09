42ab6d527d3eb4d495de00f7f49b2954fb71ef53
Joshua.H <48306359+hjx2019@users.noreply.github.com>
2021-10-08 22:48:17 -0400

    13.5.1

--- static/js/logic.js
+++ static/js/logic.js
@@ -4,8 +4,33 @@ console.log("working");
 // Create the map object with a center and zoom level.
 //let map = L.map('mapid').setView([40.7, -94.5], 4);
 // Create the map object with center at the San Francisco airport.
-let map = L.map('mapid').setView([37.6213, -122.3790], 5);
+//let map = L.map('mapid').setView([37.6213, -122.3790], 5);
 
+// 13.5.2 Create the map object with center at the San Francisco airport.
+let map = L.map('mapid').setView([37.5, -122.5], 10);
+
+// Add GeoJSON data.
+let sanFranAirport =
+{"type":"FeatureCollection","features":[{
+    "type":"Feature",
+    "properties":{
+        "id":"3469",
+        "name":"San Francisco International Airport",
+        "city":"San Francisco",
+        "country":"United States",
+        "faa":"SFO",
+        "icao":"KSFO",
+        "alt":"13",
+        "tz-offset":"-8",
+        "dst":"A",
+        "tz":"America/Los_Angeles"},
+        "geometry":{
+            "type":"Point",
+            "coordinates":[-122.375,37.61899948120117]}}
+]};
+
+// Grabbing our GeoJSON data.
+L.geoJSON(sanFranAirport).addTo(map);
 
 // Get data from cities.js
 let cityData = cities;
